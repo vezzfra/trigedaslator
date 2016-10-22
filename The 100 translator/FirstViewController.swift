600a9e81292d1754541099e6bc9a0369c717f22c
Francesco Vezzoli <francesco@MacBook-Air-di-Francesco.local>
2016-10-22 17:15:39 +0200

    Initial Commit

---
+++ The 100 translator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  The 100 translator
+//
+//  Created by Francesco Vezzoli on 22/10/16.
+//  Copyright © 2016 Ro.v.er. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The 100 translator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  The 100 translator
+//
+//  Created by Francesco Vezzoli on 22/10/16.
+//  Copyright © 2016 Ro.v.er. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
